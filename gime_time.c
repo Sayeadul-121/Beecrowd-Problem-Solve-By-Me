4a5dd7f89f008f60f13ec65ec6c64d751a602226
Sayeadul-121 <sayefbtrihs@gmail.com>
2026-09-04 12:06:24 +0600

    game time code

---
+++ gime_time.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int start, end, duration;
+
+    scanf("%d %d", &start, &end);
+
+    duration = end - start;
+
+    if (duration <= 0)
+    {
+        duration = duration + 24;
+    }
+
+    printf("O JOGO DUROU %d HORA(S)\n", duration);
+
+    return 0;
+}
